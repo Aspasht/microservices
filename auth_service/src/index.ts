1dd2c95b8c7a0a5b122cc78b26d155fb14a6991c
Aspasht <72987297+Aspasht@users.noreply.github.com>
2023-12-12 15:31:06 +0300

    added session store and local login strategy

--- auth_service/src/index.ts
+++ auth_service/src/index.ts
@@ -3,16 +3,39 @@ import cors from 'cors';
 import helmet from 'helmet';
 import 'dotenv/config';
 import { connectToDatabase } from './db/db';
-
+import MongoStore from 'connect-mongo';
+import session from 'express-session';
+import { Strategy as LocalStrategy } from 'passport-local';
+import passport from 'passport';
 const app = express();
 app.use(cors());
 app.use(helmet());
 
 
-
 // Initializes database connection
 connectToDatabase();
 
+
+// connect-mongo and express session setup 
+// storing user session on db
+
+app.use(session({
+    secret: `${process.env.SESSION_SECRET}`,
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({
+        mongoUrl: process.env.MONGO_URI
+    }),
+    cookie: { secure: true }
+}))
+
+// Initializing passportjs local-strategy
+
+app.use(passport.initialize());
+app.use(passport.session())
+
+
+// Basic server setup
 const port = process.env.PORT
 
 app.get('/', (req, res) => {
--- auth_service/src/models/userModel.ts
+++ auth_service/src/models/userModel.ts
@@ -1,17 +1,27 @@
 import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
 
-
-var userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema({
     username: { type: String, required: true },
     email: { type: String, required: true },
     password: { type: String, required: true },
 }, { timestamps: true });
 
 
-///////////////////////////////
+// hashing the password before saving to the database.
 
-// Need to hash the password before saving to the database.
-
-//////////////////////////
-
+const saltRounds = 10;
+userSchema.pre('save', function (next) {
+    if (this.isNew || this.isModified('password')) {
+        const document = this;
+        bcrypt.hash(document.password, saltRounds, function (err, hashedPassword) {
+            if (err) {
+                next(err);
+            } else {
+                document.password = hashedPassword;
+                next();
+            }
+        })
+    }
+})
 
